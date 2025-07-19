1416edfc45108630342426624b511b3011a319e9
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-20 01:56:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-// Complete the js code
-function Car(make, model){
-	this.make = make;
-	this.model = model;
+// Complete the code
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
 }
 
-function SportsCar(make, model, topSpeed){
-	Car.call(this , make, model)
-	this.topSpeed = topSpeed;
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
+};
+
+function SportsCar(make, model, topSpeed) {
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
 }
 
-Car.prototype.getMakeModel = function(){
-	return `${this.make} ${this.model}`;
-}
-
+// Correct inheritance setup
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
 
-
-SportsCar.prototype.getTopSpeed = function(){
-	return `${this.topSpeed}`;
-}
-
-const car = new SportsCar("Ferrari", "Testarossa", 200);
-console.log(car.getTopSpeed());
+// Add method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
